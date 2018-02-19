32490edd9c4071593d71282b47cd5e9a04d2f431
Lin <dorothy.lin@mail.utoronto.ca>
2018-02-19 13:27:52 -0500

    Make adjustments to access control in MapPanel.java

--- assignment2/MapPanel.java
+++ assignment2/MapPanel.java
@@ -1,24 +1,25 @@
 import java.awt.*;
 
 public class MapPanel extends Panel {
-	// ------------------------------------------------------------------
-	// The following items are used for double buffering.
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4723290168915826378L;
 
 	// The buffer in which to draw the image; used for double buffering.
-	Image backBuffer;
+	private Image backBuffer;
 
 	// The graphics context to use when double buffering.
-	Graphics backG;
+	private Graphics backG;
 
-	private Path[][] paths; // The grid of paths.
+	// The grid of paths.
+	private Path[][] paths;
 
-	public void addToPanel(Path[][] r) {
-		paths = r;
+	public MapPanel() {
+	}
+
+	public void addToPanel(Path[][] pathes) {
+		paths = pathes;
 		setLayout(new GridLayout(paths.length, paths[0].length, 0, 0));
 		for (int row = 0; row < paths.length; row++) {
 			for (int col = 0; col < paths[0].length; col++) {
@@ -30,11 +31,12 @@ public class MapPanel extends Panel {
 	// paint
 	// ------------------------------------------------------------------
 	// Paint the display.
-
+	@Override
 	public void paint(Graphics g) {
 		update(g);
 	}
 
+	@Override
 	public Insets insets() {
 		return new Insets(10, 10, 10, 10);
 	}
@@ -42,12 +44,12 @@ public class MapPanel extends Panel {
 	// update
 	// ------------------------------------------------------------------
 	// Update the display; tell all my Paths to update themselves.
-
+	@Override
 	public void update(Graphics g) {
-
+		Rectangle rec = getBounds();
 		// Get my width and height.
-		int w = bounds().width;
-		int h = bounds().height;
+		int w = rec.width;
+		int h = rec.height;
 
 		// If we don't yet have an Image, create one.
 		if (backBuffer == null || backBuffer.getWidth(null) != w
@@ -65,7 +67,6 @@ public class MapPanel extends Panel {
 		}
 
 		if (backBuffer != null) {
-
 			// Fill in the Graphics context backG.
 			g.setColor(Color.white);
 			g.fillRect(0, 0, w, h);
@@ -73,6 +74,6 @@ public class MapPanel extends Panel {
 			// Now copy the new image to g.
 			// g.drawImage(backBuffer, 0, 0, null);
 		}
-
 	}
+
 }
